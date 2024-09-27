798e2fd61f9e4500f7e7c4a83777037fdba7141d
wangjie <JerryWon1214@gmail.com>
2024-09-27 15:12:43 +0800

    feat: 添加和查找标签

---
+++ models/tag.go
@@ -0,0 +1,44 @@
+package models
+
+// Tag 标签模型
+type Tag struct {
+	Model // 继承模型
+
+	Name      string `json:"name"`       // 标签名称
+	State     int    `json:"state"`      // 标签状态
+	CreatedBy string `json:"created_by"` // 创建人
+	UpdatedBy string `json:"updated_by"` // 更新人
+}
+
+// GetTags 获取标签列表
+func GetTags(pageNum int, pageSize int, maps interface{}) (tags []Tag) {
+	db.Where(maps).Offset(pageNum).Limit(pageSize).Find(&tags)
+
+	return
+}
+
+// GetTagTotal 获取标签总数
+func GetTagTotal(maps interface{}) (count int) {
+	db.Model(&Tag{}).Where(maps).Count(&count)
+
+	return
+}
+
+// ExistTagByName 根据名称判断标签是否存在
+func ExistTagByName(name string) bool {
+	var tag Tag
+	db.Select("id").Where("name = ?", name).First(&tag)
+	if tag.ID > 0 {
+		return true
+	}
+	return false
+}
+
+// AddTag 添加标签
+func AddTag(name string, state int, createdBy string) error {
+	return db.Create(&Tag{
+		Name:      name,
+		State:     state,
+		CreatedBy: createdBy,
+	}).Error
+}
--- routers/api/v1/tag.go
+++ routers/api/v1/tag.go
@@ -1,10 +1,45 @@
 package v1
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/ZoeKyHein/go-gin-example/models"
+	"github.com/ZoeKyHein/go-gin-example/pkg/e"
+	"github.com/ZoeKyHein/go-gin-example/pkg/setting"
+	"github.com/ZoeKyHein/go-gin-example/pkg/util"
+	"github.com/astaxie/beego/validation"
+	"github.com/gin-gonic/gin"
+	"github.com/unknwon/com"
+	"net/http"
+)
 
 // AddTag 添加标签
 func AddTag(c *gin.Context) {
 	// todo 待实现
+	name := c.Query("name") // 获取参数
+	state := com.StrTo(c.DefaultQuery("state", "0")).MustInt()
+	createdBy := c.Query("created_by")
+
+	// 校验参数
+	valid := validation.Validation{}
+	valid.Required(name, "name").Message("名称不能为空")
+	valid.MaxSize(name, 100, "name").Message("名称最长为100字符")
+	valid.Required(createdBy, "created_by").Message("创建人不能为空")
+	valid.MaxSize(createdBy, 100, "created_by").Message("创建人最长为100字符")
+	valid.Range(state, 0, 1, "state").Message("状态只允许0或1")
+
+	code := e.INVALID_PARAMS
+	if !valid.HasErrors() {
+		if models.ExistTagByName(name) {
+			code = e.ERROR_EXIST_TAG
+		} else {
+			code = e.SUCCESS
+			models.AddTag(name, state, createdBy)
+		}
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"code": code,
+		"msg":  e.GetMsg(code),
+		"data": make(map[string]string),
+	})
 }
 
 // EditTag 编辑标签
@@ -20,4 +55,30 @@ func DeleteTag(c *gin.Context) {
 // GetTags 获取多个标签
 func GetTags(c *gin.Context) {
 	// todo 待实现
+	name := c.Query("name") // 获取参数
+
+	filterMaps := make(map[string]interface{}) // 存储过滤条件
+	dataMaps := make(map[string]interface{})   // 存储数据
+
+	// 筛选名称
+	if name != "" {
+		filterMaps["name"] = name
+	}
+
+	// 筛选状态
+	var state int = -1
+	if arg := c.Query("state"); arg != "" {
+		state = com.StrTo(arg).MustInt()
+		filterMaps["state"] = state
+	}
+
+	code := e.SUCCESS
+	dataMaps["lists"] = models.GetTags(util.GetPage(c), setting.PageSize, filterMaps)
+	dataMaps["total"] = models.GetTagTotal(filterMaps)
+
+	c.JSON(http.StatusOK, gin.H{
+		"code": code,
+		"msg":  e.GetMsg(code),
+		"data": dataMaps,
+	})
 }
